b48f5e66588d0942f3d9f7c299d6e2708721b5ed
DTS <76153100+DTS-11@users.noreply.github.com>
2022-01-11 16:13:26 +0300

    animated emoji

--- PizzaHat/cogs/music.py
+++ PizzaHat/cogs/music.py
@@ -6,7 +6,7 @@ import humanfriendly
 music = DiscordUtils.Music()
 
 class Music(commands.Cog):
-    """<:music:929100003178348634> Music commands"""
+    """<a:music:929100003178348634> Music commands"""
     def __init__(self, bot):
         self.bot = bot
         
