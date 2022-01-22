2d7683bb7b1246a715c05b501d26cf255f741b96
DTS <76153100+DTS-11@users.noreply.github.com>
2022-01-22 11:23:36 +0300

    Update create.sql

--- PizzaHat/create.sql
+++ PizzaHat/create.sql
@@ -1,1 +1,1 @@
-CREATE TABLE IF NOT EXISTS warnlogs (guild_id BIGINT, user_id BIGINT, reason TEXT, warns TEXT[])
+CREATE TABLE IF NOT EXISTS warnlogs (guild_id BIGINT, user_id BIGINT, reason TEXT, warns TEXT[], time DECIMAL[])
