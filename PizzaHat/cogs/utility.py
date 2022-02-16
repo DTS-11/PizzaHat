9ad4fb6c7f73ac53dbc9e3e7ec2369ae99a06ebb
Tommodev <tomm.pydev@gmail.com>
2022-02-16 15:30:06 +0100

    Uptime fix (maybe)

--- PizzaHat/cogs/utility.py
+++ PizzaHat/cogs/utility.py
@@ -11,23 +11,23 @@ start_time = time.time()
 def to_keycap(c):
     return '\N{KEYCAP TEN}' if c == 10 else str(c) + '\u20e3'
 
-def get_bot_uptime(self, *, brief=False):
-        now = datetime.datetime.utcnow()
-        delta = now - self.bot.uptime
-        hours, remainder = divmod(int(delta.total_seconds()), 3600)
-        minutes, seconds = divmod(remainder, 60)
-        days, hours = divmod(hours, 24)
+def get_bot_uptime(brief=False):
+    now = datetime.datetime.utcnow()
+    delta = now - self.bot.uptime
+    hours, remainder = divmod(int(delta.total_seconds()), 3600)
+    minutes, seconds = divmod(remainder, 60)
+    days, hours = divmod(hours, 24)
 
-        if not brief:
-            if days:
-                fmt = '{d} days, {h} hours, {m} minutes, and {s} seconds'
-            else:
-                fmt = '{h} hours, {m} minutes, and {s} seconds'
+    if not brief:
+        if days:
+            fmt = '{d} days, {h} hours, {m} minutes, and {s} seconds'
         else:
-            fmt = '{h}h {m}m {s}s'
-            if days:
-                fmt = '{d}d ' + fmt
-        return fmt.format(d=days, h=hours, m=minutes, s=seconds)
+            fmt = '{h} hours, {m} minutes, and {s} seconds'
+    else:
+        fmt = '{h}h {m}m {s}s'
+        if days:
+            fmt = '{d}d ' + fmt
+    return fmt.format(d=days, h=hours, m=minutes, s=seconds)
 
 class Utility(commands.Cog):
     """🛠️ Utility Commands"""
