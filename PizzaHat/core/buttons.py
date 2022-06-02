2bc2b1b154bfaaaa4624211d95e10c0ea08c93aa
DTS <officialdts11@gmail.com>
2022-06-02 14:16:50 +0300

    sub-classed buttons

---
+++ PizzaHat/core/buttons.py
@@ -0,0 +1,16 @@
+from discord import ButtonStyle, Emoji, PartialEmoji
+from discord.ui import Button
+from typing import Optional, Union
+
+
+class ButtonCreation(Button):
+    def __init__(
+        self,
+        *,
+        label: Optional[str] = None,
+        style: ButtonStyle = ...,
+        emoji: Optional[Union[str, Emoji, PartialEmoji]] = None,
+        url: Optional[str] = None,
+        disabled: bool = False
+    ):
+        super().__init__(label=label, style=style, emoji=emoji, url=url, disabled=disabled)
