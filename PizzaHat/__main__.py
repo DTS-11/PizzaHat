906ae9a7bc90a54e652bb716975810a7d6255ed7
DTS <officialdts11@gmail.com>
2022-06-02 15:24:59 +0300

    remove topgg post guild func

--- PizzaHat/__main__.py
+++ PizzaHat/__main__.py
@@ -1,23 +1,8 @@
 import os
 
-import topgg
-from discord.ext import tasks
 from dotenv import load_dotenv
 
 from core.bot import PizzaHat
-
-
-@tasks.loop(minutes=30)
-async def update_stats():
-    try:
-        bot.topggpy = topgg.DBLClient(bot, os.getenv("DBL_TOKEN"), autopost=True)
-        await bot.topggpy.post_guild_count()
-        print(f"Posted server count: {bot.topggpy.guild_count}")
-
-    except Exception as e:
-        print(f"Failed to post server count\n{e.__class__.__name__}: {e}")
-
-    update_stats.start()
 
 
 # run "python ." in \PizzaHat\PizzaHat\
