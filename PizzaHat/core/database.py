f42065a0d154da9aeccdd99b657f13b66b8714ba
DTS <officialdts11@gmail.com>
2022-06-05 19:26:13 +0300

    easy way to manage db

---
+++ PizzaHat/core/database.py
@@ -0,0 +1,19 @@
+import os
+import ssl
+
+import asyncpg
+from dotenv import load_dotenv
+
+load_dotenv()
+
+async def create_db_pool():
+    ssl_object = ssl.create_default_context()
+    ssl_object.check_hostname = False
+    ssl_object.verify_mode = ssl.CERT_NONE
+
+    return await asyncpg.create_pool(
+        dsn=os.getenv("DATABASE_URL"),
+        #dsn=os.getenv("DB_URL"),
+        #dsn=os.getenv("PG_URL"),
+        ssl=ssl_object
+    )
