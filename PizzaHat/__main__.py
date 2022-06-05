9a714371470cd7380cbbe6b70cada928fe02d07b
DTS <officialdts11@gmail.com>
2022-06-05 19:30:18 +0300

    new way of starting the bot

--- PizzaHat/__main__.py
+++ PizzaHat/__main__.py
@@ -3,11 +3,29 @@ import os
 from dotenv import load_dotenv
 
 from core.bot import PizzaHat
+import core.database as db
+import asyncio
 
+load_dotenv()
+
+
+async def main():
+    pool = await db.create_db_pool()
+    async with pool as db_pool:
+        print("DB Connected")
+        async with bot:
+            bot.db = db_pool
+            await bot.start(os.getenv("TOKEN"))
 
 # run "python ." in \PizzaHat\PizzaHat\
 if __name__ == '__main__':
-    load_dotenv()
     bot = PizzaHat()
-    
-    bot.run(os.getenv("TOKEN"))
+
+    try:
+        asyncio.run(main())
+
+    except ConnectionRefusedError:
+        print("DB not connected.")
+
+    except Exception as e:
+        print(e)
