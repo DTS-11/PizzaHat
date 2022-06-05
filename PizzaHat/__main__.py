37d5d8086b7347354ca33d1115c5b05ba6601ab1
DTS <officialdts11@gmail.com>
2022-06-05 20:02:09 +0300

    update main

--- PizzaHat/__main__.py
+++ PizzaHat/__main__.py
@@ -9,6 +9,7 @@ import asyncio
 load_dotenv()
 
 
+# run "python ." in \PizzaHat\PizzaHat\
 async def main():
     pool = await db.create_db_pool()
     async with pool as db_pool:
@@ -17,7 +18,7 @@ async def main():
             bot.db = db_pool
             await bot.start(os.getenv("TOKEN"))
 
-# run "python ." in \PizzaHat\PizzaHat\
+
 if __name__ == '__main__':
     bot = PizzaHat()
 
