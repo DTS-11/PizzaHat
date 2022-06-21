5959015a418fbebc638c9a57056b7e9d0aa896c4
DTS <officialdts11@gmail.com>
2022-06-21 23:31:05 +0300

    fix image commands and added new

--- PizzaHat/cogs/image.py
+++ PizzaHat/cogs/image.py
@@ -2,32 +2,26 @@ import os
 import random
 
 import aiohttp
+import alexflipnote
 import discord
 from asyncdagpi import Client, ImageFeatures
 from core.cog import Cog
 from discord.ext import commands
 
 dagpi = Client(os.getenv('DAGPI'))
+alex_api = alexflipnote.Client()
 
 
 class Images(Cog, emoji="📷"):
     """Cool image commands!"""
     def __init__(self, bot):
         self.bot = bot
-    
-    @staticmethod
-    async def generate_image(member: discord.Member, gen_name: str, username: str=None, text: str=None):
-        """Helper function for image generation commands."""
-        url = str(member.display_avatar.with_format("png").with_size(1024))
-        img = await dagpi.image_process(getattr(ImageFeatures, gen_name)(), url=url, username=username, text=text)
-        file = discord.File(fp=img.image, filename=f"{gen_name}.{img.format}")
-        
-        return file
 
     @commands.command()
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def meme(self, ctx):
         """Gets a random meme from Reddit."""
+
         async with aiohttp.ClientSession() as cs:
             async with cs.get('https://www.reddit.com/r/dankmemes/new.json?sort=hot') as r:
                 memes = await r.json()
@@ -40,30 +34,102 @@ class Images(Cog, emoji="📷"):
 
     @commands.command()
     @commands.cooldown(1, 5, commands.BucketType.user)
+    async def bird(self, ctx):
+        """Return a random bird pic or gif."""
+
+        em = discord.Embed(color=self.bot.color)
+        em.set_image(url=await alex_api.birb())
+        em.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar.url)
+
+        await ctx.send(embed=em)
+
+    @commands.command()
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def dog(self, ctx):
+        """Return a random dog pic or gif."""
+
+        em = discord.Embed(color=self.bot.color)
+        em.set_image(url=await alex_api.dogs())
+        em.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar.url)
+
+        await ctx.send(embed=em)
+
+    @commands.command()
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def cat(self, ctx):
+        """Return a random cat pic or gif."""
+
+        em = discord.Embed(color=self.bot.color)
+        em.set_image(url=await alex_api.cats())
+        em.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar.url)
+
+        await ctx.send(embed=em)
+
+    @commands.command()
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def sadcat(self, ctx):
+        """Return a random sadcat pic or gif."""
+
+        em = discord.Embed(color=self.bot.color)
+        em.set_image(url=await alex_api.sadcat())
+        em.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar.url)
+
+        await ctx.send(embed=em)
+
+    @commands.command()
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def coffee(self, ctx):
+        """Return a random coffee pic or gif."""
+
+        em = discord.Embed(color=self.bot.color)
+        em.set_image(url=await alex_api.coffee())
+        em.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar.url)
+
+        await ctx.send(embed=em)
+
+    @commands.command()
+    @commands.cooldown(1, 5, commands.BucketType.user)
     async def pixel(self, ctx, member: discord.Member=None):
         """
         Pixellates a user's avatar.
         If no user is provided, returns your avatar.
         """
+
         if member is None:
             member = ctx.author
-        await ctx.send(file=await Images.generate_image(member, "pixel"))
+
+        url = str(member.display_avatar.with_format("png").with_size(1024))
+        img = await dagpi.image_process(ImageFeatures.pixel(), url)
+        file = discord.File(fp=img.image,filename=f"pixel.{img.format}")
+
+        await ctx.send(file=file)
 
     @commands.command()
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def tweet(self, ctx, member: discord.Member, *, text):
         """Tweeting with your pfp."""
+
         uname = member.name
         text = str(text)
-        await ctx.send(file=await Images.generate_image(member, "tweet", uname, text))
+        pfp = str(member.display_avatar.with_format("png").with_size(1024))
+        img = await dagpi.image_process(ImageFeatures.tweet(), url=pfp, username=uname, text=text)   
+        file = discord.File(fp=img.image,filename=f"tweet.{img.format}")
+
+        await ctx.send(file=file)
 
     @commands.command()
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def triggered(self, ctx, member: discord.Member=None):
         """Triggers yours or someone's avatar."""
+
         if member is None:
             member = ctx.author
-        await ctx.send(file=await Images.generate_image(member, "triggered"))
+
+        pfp = str(member.display_avatar.with_format("png").with_size(1024))
+        img = await dagpi.image_process(ImageFeatures.triggered(), url=pfp)   
+        file = discord.File(fp=img.image,filename=f"triggered.{img.format}")
+
+        await ctx.send(file=file)
     
     @commands.command()
     @commands.cooldown(1, 5, commands.BucketType.user)
@@ -72,66 +138,126 @@ class Images(Cog, emoji="📷"):
         GTA wasted...
         If no user is given, returns yours.
         """
+
         if member is None:
             member = ctx.author
-        await ctx.send(file=await Images.generate_image(member, "wasted"))
+
+        pfp = str(member.display_avatar.with_format("png").with_size(1024))
+        img = await dagpi.image_process(ImageFeatures.wasted(), url=pfp)   
+        file = discord.File(fp=img.image,filename=f"wasted.{img.format}")
+
+        await ctx.send(file=file)
 
     @commands.command()
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def angel(self, ctx, member: discord.Member=None):
-        """Angelify your pfp or someone's."""
+        """Angelify your pfp or someone else's."""
+
         if member is None:
             member = ctx.author
-        await ctx.send(file=await Images.generate_image(member, "angel"))
+
+        pfp = str(member.display_avatar.with_format("png").with_size(1024))
+        img = await dagpi.image_process(ImageFeatures.angel(), url=pfp)   
+        file = discord.File(fp=img.image,filename=f"angel.{img.format}")
+
+        await ctx.send(file=file)
 
     @commands.command()
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def hitler(self, ctx, member: discord.Member=None):
         """Changes you or someone into hitler."""
+
         if member is None:
             member = ctx.author
-        await ctx.send(file=await Images.generate_image(member, "hitler"))
+
+        pfp = str(member.display_avatar.with_format("png").with_size(1024))
+        img = await dagpi.image_process(ImageFeatures.hitler(), url=pfp)   
+        file = discord.File(fp=img.image,filename=f"hitler.{img.format}")
+
+        await ctx.send(file=file)
 
     @commands.command()
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def delete(self, ctx, member: discord.Member=None):
         """Delete someone or yourself."""
+
         if member is None:
             member = ctx.author
-        await ctx.send(file=await Images.generate_image(member, "delete"))
+
+        pfp = str(member.display_avatar.with_format("png").with_size(1024))
+        img = await dagpi.image_process(ImageFeatures.delete(), url=pfp)   
+        file = discord.File(fp=img.image,filename=f"delete.{img.format}")
+
+        await ctx.send(file=file)
 
     @commands.command()
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def wanted(self, ctx, member: discord.Member=None):
         """Police wanted poster."""
+
         if member is None:
             member = ctx.author
 
-        await ctx.send(file=await Images.generate_image(member, "wanted"))
+        pfp = str(member.display_avatar.with_format("png").with_size(1024))
+        img = await dagpi.image_process(ImageFeatures.wanted(), url=pfp)   
+        file = discord.File(fp=img.image,filename=f"wanted.{img.format}")
+
+        await ctx.send(file=file)
 
     @commands.command()
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def jail(self, ctx, member: discord.Member=None):
         """Lock yourself or someone behind bars."""
+
         if member is None:
             member = ctx.author
-        await ctx.send(file=await Images.generate_image(member, "jail"))
+
+        pfp = str(member.display_avatar.with_format("png").with_size(1024))
+        img = await dagpi.image_process(ImageFeatures.jail(), url=pfp)   
+        file = discord.File(fp=img.image,filename=f"jail.{img.format}")
+
+        await ctx.send(file=file)
 
     @commands.command()
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def trash(self, ctx, member: discord.Member=None):
-        """Replace the trash with your face/avatar."""
+        """Replace the trash with your avatar."""
+
         if member is None:
             member = ctx.author
-        await ctx.send(file=await Images.generate_image(member, "trash"))
+
+        pfp = str(member.display_avatar.with_format("png").with_size(1024))
+        img = await dagpi.image_process(ImageFeatures.trash(), url=pfp)   
+        file = discord.File(fp=img.image, filename=f"trash.{img.format}")
+
+        await ctx.send(file=file)
 
     @commands.command()
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def discord(self, ctx, member: discord.Member, *, text):
         """Send a Discord message, simple."""
+        
         uname = member.name
         text = str(text)
-        await ctx.send(file=await Images.generate_image(member, "discord", uname, text))
+        pfp = str(member.display_avatar.with_format("png").with_size(1024))
+        img = await dagpi.image_process(ImageFeatures.discord(), url=pfp, username=uname, text=text)   
+        file = discord.File(fp=img.image, filename=f"discord.{img.format}")
+
+        await ctx.send(file=file)
+
+    # @commands.command()
+    # @commands.cooldown(1, 5, commands.BucketType.user)
+    # async def bonk(self, ctx, member: discord.Member):
+    #     """"""
+
+    #     if member is None:
+    #         member = ctx.author
+
+    #     pfp = str(member.display_avatar.with_format("png").with_size(1024))
+    #     img = await dagpi.image_process(ImageFeatures.bonk(), url=pfp)
+    #     file = discord.File(fp=img.image, filename=f"bonk.{img.format}")
+
+    #     await ctx.send(file=file)
 
 
 async def setup(bot):
