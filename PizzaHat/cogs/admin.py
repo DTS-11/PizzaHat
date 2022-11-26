3e47f960ef70d9415cf39ba8147777bd5d752458
DTS <officialdts11@gmail.com>
2022-11-26 13:54:24 +0530

    remove some stuff

--- PizzaHat/cogs/admin.py
+++ PizzaHat/cogs/admin.py
@@ -4,8 +4,6 @@ from core.cog import Cog
 from discord.ext import commands
 from discord.ext.commands import Context
 
-from .tickets import TicketView
-
 
 class Admin(Cog, emoji=916988537264570368):
     """Admin configuration commands."""
@@ -19,19 +17,6 @@ class Admin(Cog, emoji=916988537264570368):
     async def set(self, ctx: Context):
         if ctx.subcommand_passed is None:
             await ctx.send_help(ctx.command)
-
-    @set.command(aliases=['modrole'])
-    @commands.has_permissions(manage_guild=True)
-    @commands.bot_has_permissions(manage_guild=True)
-    @commands.cooldown(1, 10, commands.BucketType.user)
-    async def staffrole(self, ctx: Context, role: discord.Role):
-        """Set a staff/mod-role."""
-
-        try:
-            await self.bot.db.execute("INSERT INTO staff_role (guild_id, role_id) VALUES ($1, $2)", ctx.guild.id, role.id)  # type: ignore
-        
-        except Exception as e:
-            print(e)
 
     @set.command(aliases=['log'])
     @commands.has_permissions(manage_guild=True)
@@ -49,25 +34,6 @@ class Admin(Cog, emoji=916988537264570368):
 
         except Exception as e:
             print(e)
-
-    @set.command(aliases=['ticket'])
-    @commands.has_permissions(manage_guild=True)
-    @commands.bot_has_permissions(manage_guild=True)
-    @commands.cooldown(1, 10, commands.BucketType.user)
-    async def tickets(self, ctx: Context, channel: discord.TextChannel):
-        """
-        Set up the Tickets system in the server by 
-        sending the `Create Ticket` message.
-        """
-
-        em = discord.Embed(
-            title="Create a ticket!",
-            description="Click <:ticket_emoji:1004648922158989404> to create/open a new ticket.",
-            color=discord.Color.gold()
-        )
-        em.set_thumbnail(url="https://i.imgur.com/mOTlTBy.png")
-
-        await channel.send(embed=em, view=TicketView(self.bot))
 
     @commands.group(invoke_without_command=True)
     @commands.has_permissions(manage_guild=True)
