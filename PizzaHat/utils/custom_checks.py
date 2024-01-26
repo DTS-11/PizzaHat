84fd4647412997852f4209c11893fa40188cdc34
DTS <officialdts11@gmail.com>
2024-01-26 15:38:38 +0530

    some more checks

--- PizzaHat/utils/custom_checks.py
+++ PizzaHat/utils/custom_checks.py
@@ -2,17 +2,36 @@ from discord.ext import commands
 from discord.ext.commands import Context
 
 
-class NotServerStaff(commands.CheckFailure):
+class NoStaffRoleSet(commands.CheckFailure):
     pass
 
 
-def is_staff():
-    def predicate(ctx: Context):
-        val = ctx.bot.db.fetchval("SELECT role_id FROM staff_role WHERE guild_id=$1", ctx.guild.id)  # type: ignore
+class UserNotStaff(commands.CheckFailure):
+    pass
 
-        if val:
+
+def server_staff_role():
+    def predicate(ctx: Context):
+        role_id = ctx.bot.db.fetchval("SELECT role_id FROM staff_role WHERE guild_id=$1", ctx.guild.id)  # type: ignore
+        staff_role = ctx.guild.get_role(role_id)  # type: ignore
+
+        if staff_role:
             return True
+
         else:
-            raise NotServerStaff
+            raise NoStaffRoleSet
+
+    return commands.check(predicate)
+
+
+def user_is_staff():
+    def predicate(ctx: Context):
+        role_id = ctx.bot.db.fetchval("SELECT role_id FROM staff_role WHERE guild_id=$1", ctx.guild.id)  # type: ignore
+
+        if role_id in ctx.author.roles:  # type: ignore
+            return True
+
+        else:
+            raise UserNotStaff
 
     return commands.check(predicate)
