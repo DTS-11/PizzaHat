335bc0c99213af3a314da33e864442b734b0c33b
DTS <officialdts11@gmail.com>
2024-02-27 15:01:56 +0530

    update cog name, update command group name

--- PizzaHat/cogs/starboard.py
+++ PizzaHat/cogs/starboard.py
@@ -5,16 +5,16 @@ from discord.ext import commands
 from discord.ext.commands import Context
 
 
-class Stars(Cog, emoji="⭐"):
+class Starboard(Cog, emoji="⭐"):
     """A starboard system to upvote messages."""
 
     def __init__(self, bot: PizzaHat):
         self.bot: PizzaHat = bot
 
-    @commands.group(aliases=["star"])
+    @commands.group()
     @commands.guild_only()
     @commands.has_permissions(manage_guild=True)
-    async def starboard(self, ctx: Context):
+    async def star(self, ctx: Context):
         """
         Starboard commands.
 
@@ -24,11 +24,11 @@ class Stars(Cog, emoji="⭐"):
         if ctx.subcommand_passed is None:
             await ctx.send_help(ctx.command)
 
-    @starboard.command(name="channel")
+    @star.command(name="channel")
     @commands.guild_only()
     @commands.has_permissions(manage_guild=True)
     @commands.cooldown(1, 60, commands.BucketType.user)
-    async def starboard_channel(self, ctx: Context, channel: discord.TextChannel):
+    async def star_channel(self, ctx: Context, channel: discord.TextChannel):
         """
         Set the starboard channel.
         To replace this channel, simply run this command again.
@@ -37,7 +37,15 @@ class Stars(Cog, emoji="⭐"):
         """
 
         try:
-            await self.bot.db.execute("INSERT INTO starboard (guild_id, channel_id) VALUES ($1, $2) ON CONFLICT (guild_id) DO UPDATE SET channel_id=$2", ctx.guild.id, channel.id)  # type: ignore
+            (
+                await self.bot.db.execute(
+                    "INSERT INTO starboard (guild_id, channel_id) VALUES ($1, $2) ON CONFLICT (guild_id) DO UPDATE SET channel_id=$2",
+                    ctx.guild.id,
+                    channel.id,
+                )
+                if self.bot.db and ctx.guild
+                else None
+            )
             await ctx.send(
                 f"{self.bot.yes} Starboard channel set to {channel.mention}."
             )
@@ -46,47 +54,64 @@ class Stars(Cog, emoji="⭐"):
             await ctx.send(f"{self.bot.no} Something went wrong...")
             print(e)
 
-    @starboard.command(name="count", aliases=["limit"])
+    @star.command(name="count", aliases=["limit"])
     @commands.guild_only()
     @commands.has_permissions(manage_guild=True)
     @commands.cooldown(1, 60, commands.BucketType.user)
-    async def starboard_count(self, ctx: Context, count: int):
+    async def star_count(self, ctx: Context, count: int):
         """
-        Set the starboard star count. 
-        Default count is set to 5.
-        Minimum limit is 3 and maximum limit is 100.
+        Set the starboard star count.
+        Default count is set to 5. Maximum limit is 100.
 
         To use this command, you need Manage Server permission.
         """
 
         try:
-            # if count < 3:
-            #     return await ctx.send(f"{self.bot.no} Minimum limit is 3.")
+            if count > 100:
+                return await ctx.send(f"{self.bot.no} Maximum limit is 100.")
 
-            # elif count > 100:
-            #     return await ctx.send(f"{self.bot.no} Maximum limit is 100.")
-
-            await self.bot.db.execute("INSERT INTO starboard (guild_id, star_count) VALUES ($1, $2) ON CONFLICT (guild_id) DO UPDATE SET star_count=$2", ctx.guild.id, count)  # type: ignore
+            (
+                await self.bot.db.execute(
+                    "INSERT INTO starboard (guild_id, star_count) VALUES ($1, $2) ON CONFLICT (guild_id) DO UPDATE SET star_count=$2",
+                    ctx.guild.id,
+                    count,
+                )
+                if self.bot.db and ctx.guild
+                else None
+            )
             await ctx.send(f"{self.bot.yes} Starboard star count set to `{count}`.")
 
         except Exception as e:
             await ctx.send(f"{self.bot.no} Something went wrong...")
             print(e)
 
-    @starboard.command(name="self")
+    @star.command(name="self")
     @commands.guild_only()
     @commands.has_permissions(manage_guild=True)
     @commands.cooldown(1, 60, commands.BucketType.user)
-    async def starboard_self(self, ctx: Context, enable: bool):
+    async def star_self(self, ctx: Context, enable: bool):
         """
-        Toggle self star. 
-        Defaults to True.
+        Toggle self star.
+        Accepts true/false values. Defaults to True.
 
         To use this command, you need Manage Server permission.
         """
 
         try:
-            await self.bot.db.execute("INSERT INTO starboard (guild_id, self_star) VALUES ($1, $2) ON CONFLICT (guild_id) DO UPDATE SET self_star=$2", ctx.guild.id, enable)  # type: ignore
+            if enable not in (True, False):
+                return await ctx.send(
+                    f"{self.bot.no} Please enter a valid value. Accepts `true` or `false`."
+                )
+
+            (
+                await self.bot.db.execute(
+                    "INSERT INTO starboard (guild_id, self_star) VALUES ($1, $2) ON CONFLICT (guild_id) DO UPDATE SET self_star=$2",
+                    ctx.guild.id,
+                    enable,
+                )
+                if self.bot.db and ctx.guild
+                else None
+            )
             await ctx.send(
                 f"{self.bot.yes} Starboard self-star set to `{'true' if enable else 'false'}`."
             )
@@ -97,4 +122,4 @@ class Stars(Cog, emoji="⭐"):
 
 
 async def setup(bot):
-    await bot.add_cog(Stars(bot))
+    await bot.add_cog(Starboard(bot))
