1a98bcd8674152f4c0470e7d0d309243a2ee325c
DTS <officialdts11@gmail.com>
2024-03-21 20:26:37 +0530

    update tickets

--- PizzaHat/cogs/tickets.py
+++ PizzaHat/cogs/tickets.py
@@ -14,49 +14,38 @@ class TicketView(ui.View):
         self.thread_id = None
         super().__init__(timeout=None)
 
-    @alru_cache()
-    async def get_staff_role(self, guild_id: int) -> int:
-        return await self.bot.db.fetchval(
-            "SELECT role_id FROM staff_role WHERE guild_id=$1", guild_id
-        )
-
     @ui.button(
         emoji="<:ticket_emoji:1004648922158989404>", custom_id="create_ticket_btn"
     )
     @commands.bot_has_permissions(create_private_threads=True)
     async def create_ticket(self, interaction: Interaction, button: ui.Button):
-        if interaction.guild is not None:
-            staff_role = interaction.guild.get_role(
-                await self.get_staff_role(interaction.guild.id)
-            )
+        if not interaction.guild:
+            return
 
-            if staff_role is not None:
-                thread = await interaction.channel.create_thread(  # type: ignore
-                    name=f"ticket-{interaction.user}",
-                    reason=f"Ticket created by {interaction.user}",
-                    invitable=False,  # type: ignore
-                )
-                await thread.add_user(interaction.user)
+        thread = await interaction.channel.create_thread(  # type: ignore
+            name=f"ticket-{interaction.user}",
+            reason=f"Ticket created by {interaction.user}",
+            invitable=False,  # type: ignore
+        )
+        await thread.add_user(interaction.user)
 
-                em = discord.Embed(
-                    title="Ticket created!",
-                    description=f"Welcome {interaction.user.mention} `[{interaction.user}]`. Support team will get back to you shortly.",
-                    color=self.bot.color,
-                    timestamp=interaction.created_at,
-                )
-                em.set_footer(
-                    text=interaction.user,
-                    icon_url=(
-                        interaction.user.avatar.url if interaction.user.avatar else None
-                    ),
-                )
+        em = discord.Embed(
+            title="Ticket created!",
+            description=f"Welcome {interaction.user.mention} `[{interaction.user}]`. Support team will get back to you shortly.",
+            color=self.bot.color,
+            timestamp=interaction.created_at,
+        )
+        em.set_footer(
+            text=interaction.user,
+            icon_url=(interaction.user.avatar.url if interaction.user.avatar else None),
+        )
 
-                await thread.send(
-                    content=f"{interaction.user.mention}",
-                    embed=em,
-                    view=TicketSettings(thread.id),
-                )
-                self.thread_id = thread.id
+        await thread.send(
+            content=f"{interaction.user.mention}",
+            embed=em,
+            view=TicketSettings(thread.id),
+        )
+        self.thread_id = thread.id
 
 
 class TicketSettings(ui.View):
@@ -102,20 +91,35 @@ class TicketSettings(ui.View):
 
 
 class Tickets(Cog, emoji="🎟"):
-    """Ticket system."""
+    """Button ticket system for support and help!"""
 
     def __init__(self, bot: PizzaHat):
         self.bot: PizzaHat = bot
 
     @commands.command(aliases=["tickets"])
-    @commands.cooldown(1, 10, commands.BucketType.user)
-    @commands.has_permissions(manage_guild=True)
-    async def ticket(self, ctx: Context):
-        """Set up ticket system."""
+    @commands.guild_only()
+    @commands.has_permissions(manage_channels=True)
+    @commands.bot_has_permissions(manage_channels=True)
+    @commands.cooldown(1, 60, commands.BucketType.user)
+    async def ticket(self, ctx: Context, channel: discord.TextChannel):
+        """
+        Set up the Ticket system in the server by sending the `Create Ticket` message.
 
-        await ctx.send(
-            "Please use the command `p!setup tickets <#channel>` to setup ticket system in this server."
+        In order for this to work, the bot must have Manage Server permissions.
+
+        To use this command, you must have Manage Server permission.
+        """
+
+        em = discord.Embed(
+            title="Create a ticket!",
+            description="Click <:ticket_emoji:1004648922158989404> to create/open a new ticket.",
+            color=discord.Color.gold(),
         )
+        em.set_thumbnail(url="https://i.imgur.com/mOTlTBy.png")
+
+        view = TicketView(self.bot)
+        await channel.send(embed=em, view=view)
+        await ctx.message.add_reaction(self.bot.yes)
 
 
 async def setup(bot):
