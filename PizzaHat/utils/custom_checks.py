553dc03e600aef36741768b408ed8e89cb08bdc2
DTS <officialdts11@gmail.com>
2024-03-21 20:26:12 +0530

    comment everything

--- PizzaHat/utils/custom_checks.py
+++ PizzaHat/utils/custom_checks.py
@@ -1,44 +1,44 @@
-from discord.ext import commands
-from discord.ext.commands import Context
+# from discord.ext import commands
+# from discord.ext.commands import Context
 
 
-class NoStaffRoleSet(commands.CheckFailure):
-    pass
+# class NoStaffRoleSet(commands.CheckFailure):
+#     pass
 
 
-class UserNotStaff(commands.CheckFailure):
-    pass
+# class UserNotStaff(commands.CheckFailure):
+#     pass
 
 
-def server_staff_role():
-    """Check if the server has a staff role set."""
+# def server_staff_role():
+#     """Check if the server has a staff role set."""
 
-    async def predicate(ctx: Context):
-        role_id = await ctx.bot.db.fetchval("SELECT role_id FROM staff_role WHERE guild_id=$1", ctx.guild.id)  # type: ignore
+#     async def predicate(ctx: Context):
+#         role_id = await ctx.bot.db.fetchval("SELECT role_id FROM staff_role WHERE guild_id=$1", ctx.guild.id)  # type: ignore
 
-        if ctx.guild.get_role(role_id):  # type: ignore
-            return True
+#         if ctx.guild.get_role(role_id):  # type: ignore
+#             return True
 
-        else:
-            await ctx.send("No staff role set in this server.")
-            raise NoStaffRoleSet()
+#         else:
+#             await ctx.send("No staff role set in this server.")
+#             raise NoStaffRoleSet()
 
-    return commands.check(predicate)
+#     return commands.check(predicate)
 
 
-def user_is_staff():
-    """Check if the user has a staff role."""
+# def user_is_staff():
+#     """Check if the user has a staff role."""
 
-    async def predicate(ctx: Context):
-        role_id = await ctx.bot.db.fetchval("SELECT role_id FROM staff_role WHERE guild_id=$1", ctx.guild.id)  # type: ignore
+#     async def predicate(ctx: Context):
+#         role_id = await ctx.bot.db.fetchval("SELECT role_id FROM staff_role WHERE guild_id=$1", ctx.guild.id)  # type: ignore
 
-        if role_id in [role.id for role in ctx.author.roles]:  # type: ignore
-            return True
+#         if role_id in [role.id for role in ctx.author.roles]:  # type: ignore
+#             return True
 
-        else:
-            await ctx.send(
-                "You do not have the required staff role to use this command."
-            )
-            raise UserNotStaff()
+#         else:
+#             await ctx.send(
+#                 "You do not have the required staff role to use this command."
+#             )
+#             raise UserNotStaff()
 
-    return commands.check(predicate)
+#     return commands.check(predicate)
