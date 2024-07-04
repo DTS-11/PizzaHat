b6b4bfa3c5e48e4555594dd2b21d5dd7cc532a2e
DTS <officialdts11@gmail.com>
2024-07-04 13:34:18 +0530

    update config file for easier setup

--- PizzaHat/utils/config.py
+++ PizzaHat/utils/config.py
@@ -1,3 +1,19 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+LOGS_CHANNEL = 980151632199299092  # channel for sending bot join/leave logs
+
+TOKEN = os.getenv("BOT_TOKEN")  # bot token
+TEST_BOT = os.getenv("TEST_BOT")  # test bot token (optional)
+PG_URL = os.getenv("PG_URL")  # postgres connection url
+TOPGG_TOKEN = os.getenv("DBL_TOKEN")  # top.gg token
+DLIST_TOKEN = os.getenv("DLIST_AUTH")  # dlist.gg token
+
+
 COG_EXCEPTIONS = [
     "AntiAltsConfig",
     "AutoModConfig",
