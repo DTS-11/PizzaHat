c9b93f1bd6cb615b1b0d0ed5a40e522731291aca
DTS <officialdts11@gmail.com>
2026-05-20 22:36:05 +0530

    pushed test bot again 🤦

--- PizzaHat/__main__.py
+++ PizzaHat/__main__.py
@@ -1,10 +1,10 @@
 import logging
 
 from core.bot import PizzaHat
-from utils.config import TEST_BOT
+from utils.config import TOKEN
 
 logger = logging.getLogger("bot")
 
 if __name__ == "__main__":
     bot = PizzaHat()
-    bot.run(TEST_BOT, root_logger=True)  # type: ignore
+    bot.run(TOKEN, root_logger=True)  # type: ignore
